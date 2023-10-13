f0bab86fc1f6002356b6be67f3d56db54d091f2d
Victor Santos (vct) <victor_santos@fisica.ufc.br>
2023-10-13 17:16:57 -0300

    Fix rgamma
    Due to overflow, change GSL to ARB

--- src/sf.c
+++ src/sf.c
@@ -5,6 +5,8 @@
 #include "log.h"
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_sf.h>
+#include <arb.h>
+#include <arb_hypgeom.h>
 
 num_t
 num_erfc (const num_t z)
@@ -26,14 +28,17 @@ num_t
 num_rgamma (const num_t z)
 {
     assert(num_is_real(z));
-    const double x = num_to_double(z);
-        
-    gsl_sf_result result;
 
-    int status = gsl_sf_gammainv_e(x, &result);
+    const int prec = 53;
+    arb_t res, x;    
+    arb_init(res); arb_init(x);
+    arb_set_d(x, num_to_double(z));
+    arb_hypgeom_rgamma(res, x,  prec);
+    const double ret = arf_get_d(arb_midref(res), ARF_RND_NEAR) ;
+    arb_clear(res); arb_clear(x);
 
-    log_debug("status  = %s\n", gsl_strerror(status));
-    log_debug ("1/Gamma(%g) = %.6e +/- % .6e\n",
-               x, result.val, result.err);
-    return new(num, result.val, 0.0);
+    log_debug("1/Gamma(%g) = %.6e\n", num_to_double(z), ret);
+
+    
+    return new(num, ret, 0.0);
 }
